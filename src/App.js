6bb8696e54861cad4da361bf5feec07c8565ea0f
Elías Heresi <elias.heresi@gmail.com>
2022-05-02 19:34:26 +0200

    Created API extraction function, adding it to the AlbumContextProvider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
+import { useContext } from "react";
 import Header from "./component/Header/Header";
+import AlbumContext from "./contexts/Album/AlbumContext";
 
 function App() {
+  const { albums } = useContext(AlbumContext);
   return (
     <>
       <Header />
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import AlbumContextProvider from "./contexts/Album/AlbumContextProvider";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <AlbumContextProvider>
+      <App />
+    </AlbumContextProvider>
   </React.StrictMode>
 );
 
