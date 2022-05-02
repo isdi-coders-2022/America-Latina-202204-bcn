5a43676c05e62f0ff5cc197467e4356daf9ee72e
maicol torres <maicolt401@gmail.com>
2022-05-02 21:42:00 +0200

    elaborate testing for header

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -3,22 +3,26 @@ import styled from "styled-components";
 const Navigation = styled.nav`
   background: #fd6262;
   display: flex;
+  flex-direction: column;
   position: sticky;
   display: flex;
-  justify-content: space-between;
-  align-items: center;
   height: 60px;
   top: 0;
   right: 0px;
   left: 0px;
+  text-align: center;
 
   ul {
     display: flex;
     list-style: none;
     justify-content: space-around;
     padding-left: 0px;
+    margin-top: 0px;
   }
   li {
+    flex-direction: column;
+    text-align: center;
+    justify-content: space-between;
     height: 55px;
     padding: 2px;
     display: flex;
@@ -26,7 +30,6 @@ const Navigation = styled.nav`
   }
 
   a {
-    align-content: center;
     text-decoration: none;
     color: black;
     padding: 5px;
---
+++ src/component/Header/Header.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import Header from "./Header";
+
+describe("Given a  component", () => {
+  describe("When it receives a text containing 'My Collection'", () => {
+    test("Then it should show the text in a nav element", () => {
+      const inputText = "My Collectiones";
+      render(<Header text={inputText} />);
+
+      const searchedResult = screen.getByText(inputText);
+      expect(searchedResult).toBeInTheDocument();
+    });
+  });
+});
