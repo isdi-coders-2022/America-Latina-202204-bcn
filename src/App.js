382b15af0beec0fe1fa55a2af54f29db546c4660
Elías Heresi <elias.heresi@gmail.com>
2022-05-02 19:38:32 +0200

    Eliminated unused imports

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
-import { useContext } from "react";
 import Header from "./component/Header/Header";
-import AlbumContext from "./contexts/Album/AlbumContext";
 
 function App() {
-  const { albums } = useContext(AlbumContext);
   return (
     <>
       <Header />
