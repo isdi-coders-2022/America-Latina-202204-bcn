60f80557ea28744f40664324827592a07902f6bd
Emanuele Panesi <emanuele.panesi@gmail.com>
2022-05-03 14:10:07 +0200

    added button test

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import Button from "./component/Button/Button";
 import Footer from "./component/Footer/Footer";
+
 import Header from "./component/Header/Header";
 
 function App() {
   return (
     <>
       <Header />
-      <Button type="form" text="hola" />
+
       <Footer />
     </>
   );
