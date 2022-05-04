aab6c4da8a0d08bffabbb1e41e4779682fa11bf7
Elías Heresi <elias.heresi@gmail.com>
2022-05-04 15:49:39 +0200

    Tested modifyAlbumFunction

--- src/store/reducers/albumsReducer.test.js
+++ src/store/reducers/albumsReducer.test.js
@@ -2,6 +2,7 @@ import albums from "../contexts/basicAlbums";
 import {
   addAlbumsAction,
   deleteAlbumsAction,
+  modifyAlbumsAction,
 } from "../actions/AlbumsActionCreator";
 import albumReducer from "./albumsReducer";
 describe("Given the addAlbumFunction passed on to the reducer and an initial array with one album", () => {
@@ -17,7 +18,21 @@ describe("Given the addAlbumFunction passed on to the reducer and an initial arr
   });
 });
 
-describe("Given the deleteAlbumFunction passed on to the reducer and an album to identificator", () => {
+describe("Given the modifyAlbumFunction passed on to the reducer", () => {
+  describe("When the identificator Linkin Park-Minutes to Midnight and new property {created-property: changed-property}", () => {
+    test("Then it will return an array which contains the modified object", () => {
+      const originalArray = albums;
+      const albumToBeModified = "Linkin Park-Minutes to Midnight";
+      const newProperty = { "created-property": "changed-property" };
+      const usedAction = modifyAlbumsAction(albumToBeModified, newProperty);
+      const newArray = albumReducer(originalArray, usedAction);
+      const expectedArray = [newProperty];
+      expect(newArray).toEqual(expectedArray);
+    });
+  });
+});
+
+describe("Given the modifyAlbumFunction passed on to the reducer, the properties {new-property} and the identificator Linkin Park-Minutes to Midnight", () => {
   describe("When the object {title: 'New added album'} is passed on to the reducer", () => {
     test("Then it will return a new array which extends the initial array to having the new object", () => {
       const originalArray = albums;
