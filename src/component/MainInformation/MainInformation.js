35cf79d578570f33cf3bb4fd6c10d5c56a97ea60
maicol torres <maicolt401@gmail.com>
2022-05-05 22:22:14 +0200

    delete text on maininformation

--- src/component/MainInformation/MainInformation.js
+++ src/component/MainInformation/MainInformation.js
@@ -8,11 +8,6 @@ const Image = styled.img`
     display: none;
   }
 `;
-const Informatio = styled.div`
-  @media (max-width: 600px) {
-    display: none;
-  }
-`;
 
 const MainInformation = () => {
   return (
@@ -21,21 +16,6 @@ const MainInformation = () => {
         src="img/persona-escuchando-musica.jpg"
         alt="personas alegres escuchadno musica"
       />
-      <Informatio>
-        <p>hola</p>
-        <p>
-          Bienvenido a la colección de álbumes que marcaron época en la música y
-          que no pueden faltar en la playlist de un Hombre Esquire. ¿Cuántos has
-          escuchado?
-        </p>
-        <p>
-          Seguro que muchos de los integrantes de este top no te sorprenden, no
-          vamos a descubrir la pólvora a estas alturas ni menospreciar a The
-          Beatles o Elvis Presley. Pero quizá alguno te sorprenda o haga que lo
-          revisites con una sonrisa en los labios mientras subes un poquito más
-          el volumen. Eso estaría muy bien.
-        </p>
-      </Informatio>
     </>
   );
 };
