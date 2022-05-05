d8d4be87dd1fa54427bf2fa3844bf85ebb70bf49
Elías Heresi <elias.heresi@gmail.com>
2022-05-05 16:06:02 +0200

    Add albums to collection function

--- src/pages/MyPlaylist/MyPlaylistPage.test.js
+++ src/pages/MyPlaylist/MyPlaylistPage.test.js
@@ -1,12 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import MyPlaylistPage from "./MyPlaylistPage";
+import AlbumProvider from "../../store/contexts/AlbumProvider";
 
 describe("Given a DetailPage Component", () => {
   describe("Whne it receives a text content `My Playlist`", () => {
-    test("Then it showl show a text `My Playlist`", () => {
+    test("Then it should show a text `My Playlist`", () => {
       const textResult = "My Playlist";
 
-      render(<MyPlaylistPage />);
+      render(
+        <AlbumProvider>
+          <MyPlaylistPage />
+        </AlbumProvider>
+      );
 
       const searchResult = screen.getByText(textResult);
       expect(searchResult).toBeInTheDocument();
